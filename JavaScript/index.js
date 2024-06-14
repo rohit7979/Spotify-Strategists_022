132c31c53282f7fcbd76a7e92f1d4dcd33f601b3
Arnab Baruah <baruaharnab769@gmail.com>
2024-06-14 07:59:55 +0530

    added dynamic sorting and search feature

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,20 +1,20 @@
-let finaldata = JSON.parse(localStorage.getItem('products')) || [];
-let user = JSON.parse(localStorage.getItem('user')) || {};
+let finaldata = JSON.parse(localStorage.getItem("products")) || [];
+let user = JSON.parse(localStorage.getItem("user")) || {};
 let arr;
-let filtervalue = localStorage.getItem('filtervalue');
-let s1 = document.getElementById('s1');
+let filtervalue = localStorage.getItem("filtervalue");
+let s1 = document.getElementById("s1");
 
 if (user && user.isloggedin) {
-  const status = document.querySelector('#s1');
-  status.textContent = 'Sign Out.';
-  status.innerText === 'Sign Out.' &&
-    status.addEventListener('click', () => {
-      localStorage.removeItem('user');
-      status.textContent = 'Sign In.';
+  const status = document.querySelector("#s1");
+  status.textContent = "Sign Out.";
+  status.innerText === "Sign Out." &&
+    status.addEventListener("click", () => {
+      localStorage.removeItem("user");
+      status.textContent = "Sign In.";
     });
 } else {
-  const status = document.querySelector('#s1');
-  status.textContent = 'Sign In.';
+  const status = document.querySelector("#s1");
+  status.textContent = "Sign In.";
 }
 
 async function fetchProducts(url) {
@@ -24,64 +24,58 @@ async function fetchProducts(url) {
     display(data);
     finaldata = data;
     arr = data;
-    localStorage.setItem('products', JSON.stringify(data));
+    localStorage.setItem("products", JSON.stringify(data));
 
     console.log(filtervalue);
-    if (filtervalue) {
-      displayCards(filtervalue);
-      localStorage.setItem('filtervalue', '');
-    }
   } catch (error) {
     console.log(error);
   }
 }
 
-let matotalvalue = document.getElementById('matotalvalue');
+let newtotalvalue = document.getElementById("newtotalvalue");
 
 function display(data) {
-  matotalvalue.innerHTML = `<p>${data.length} Items</p>`;
-  macardbox.innerHTML = '';
+  newtotalvalue.innerHTML = `<p>${data.length} Items</p>`;
+  newcardbox.innerHTML = "";
   data.forEach((item) => {
-    macardbox.append(cardCreater(item));
+    newcardbox.append(cardCreater(item));
   });
 }
-fetchProducts('https://teesta-argument-014.onrender.com/data');
+fetchProducts("http://localhost:3000/data");
 
-let macardbox = document.getElementById('macardbox');
+let newcardbox = document.getElementById("newcardbox");
 
 function cardCreater(item) {
-  let div = document.createElement('div');
-  div.className = 'cardstyle';
-  let image = document.createElement('img');
-  let designer = document.createElement('p');
-  designer.className = 'maptags';
-  let title = document.createElement('p');
-  let price = document.createElement('p');
-  title.className = 'maptags';
-  price.className = 'maptags';
+  let div = document.createElement("div");
+  div.className = "cardstyle";
+  let image = document.createElement("img");
+  let designer = document.createElement("p");
+  designer.className = "maptags";
+  let title = document.createElement("p");
+  let price = document.createElement("p");
+  title.className = "maptags";
+  price.className = "maptags";
 
-  image.setAttribute('src', item.image);
+  image.setAttribute("src", item.image);
   designer.innerText = item.designer;
   title.innerText = item.title;
   price.innerText = item.price;
   div.append(image, designer, title, price);
-  div.addEventListener('click', () => {
-    localStorage.setItem('clickeditem', JSON.stringify(item));
-    window.location.href = 'productdisplay.html';
+  div.addEventListener("click", () => {
+    localStorage.setItem("clickeditem", JSON.stringify(item));
+    window.location.href = "productdisplay.html";
   });
   return div;
 }
 function redirect(value) {
-  localStorage.setItem('filtervalue', value);
-  window.location.href = 'new.html';
+  window.location.href = "new.html";
 }
 function displayCards(val) {
-  localStorage.setItem('filtervalue', val);
   console.log(val);
   let arr1;
-  if (val === 'new') {
+  if (val === "new") {
     arr1 = arr.filter((ele) => {
-      if (ele.new == 'true') {
+      if (ele.new == "true") {
         return true;
       }
     });
@@ -89,14 +83,14 @@ function displayCards(val) {
     display(arr1);
   } else {
     arr1 = arr.filter((ele) => {
-      if (ele.category == val) {
+      if (ele.category === val) {
         return true;
       }
     });
     console.log(arr1);
     display(arr1);
   }
-  localStorage.setItem('pagearr', JSON.stringify(arr1));
+  localStorage.setItem("pagearr", JSON.stringify(arr1));
 }
 
 let designers = finaldata.map((item) => {
@@ -104,63 +98,70 @@ let designers = finaldata.map((item) => {
 });
 let finaldesigners = [...new Set(designers)];
 
-let selection = document.getElementById('selection');
+let selection = document.getElementById("selection");
 finaldesigners.sort((a, b) => {
   return a - b;
 });
 
 finaldesigners.forEach((ele) => {
-  let option = document.createElement('option');
-  option.className = 'optionfont';
+  let option = document.createElement("option");
+  option.className = "optionfont";
   option.value = ele;
   option.textContent = ele.toUpperCase();
   selection.appendChild(option);
 });
 
-selection.addEventListener('change', (e) => {
+selection.addEventListener("change", (e) => {
   let ans1 = finaldata.filter((ele) => {
     return ele.designer == selection.value;
   });
   display(ans1);
 });
 
-let sorter1 = document.getElementById('sorter');
+let sorter1 = document.getElementById("sorter");
 
-sorter1.addEventListener('change', () => {
-  let pagearr = JSON.parse(localStorage.getItem('pagearr'));
+sorter1.addEventListener("change", (e) => {
+  let pagearr = JSON.parse(localStorage.getItem("pagearr"));
   console.log(pagearr);
-  let val = localStorage.getItem('val');
+  let val = e.target.value;
   console.log(val);
-  if (sorter1.value == 'low') {
-    pagearr.sort((a, b) => {
+  if (sorter1.value == "low") {
+    arr.sort((a, b) => {
       return a.price - b.price;
     });
 
-    display(pagearr);
-  } else if (sorter1.value == 'high') {
-    pagearr.sort((a, b) => {
+    display(arr);
+  } else if (sorter1.value == "high") {
+    arr.sort((a, b) => {
       return b.price - a.price;
     });
-    display(pagearr);
-  } else if (sorter1.value == 'designer') {
-    pagearr.sort((a, b) => {
+    display(arr);
+  } else if (sorter1.value == "designer") {
+    arr.sort((a, b) => {
       return a.designer.localeCompare(b.designer);
     });
-    display(pagearr);
+    display(arr);
   }
 });
 
-let searchname = document.getElementById('searchname');
-let searchbutton = document.getElementById('searchbutton');
+let searchname = document.getElementById("searchname");
+let searchbutton = document.getElementById("searchbutton");
 
-searchbutton.addEventListener('click', (e) => {
+searchbutton.addEventListener("click", (e) => {
   e.preventDefault();
   let value = searchname.value;
-  let pagearr = JSON.parse(localStorage.getItem('pagearr'));
+  let pagearr = arr;
+
+  console.log(value);
+  console.log(pagearr);
 
   let ans1 = pagearr.filter((ele) => {
-    return ele.title.toLowerCase().includes(value);
+    return (
+      ele.title.toLowerCase().includes(value) ||
+      ele.designer.toLowerCase().includes(value)
+    );
   });
+  console.log(ans1);
   display(ans1);
-  searchname.value = '';
+  searchname.value = "";
 });
